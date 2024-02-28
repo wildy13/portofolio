63565c63a64364c237f4acf6c8132dee6fd90dae
wildy13 <wildy.simanjuntak13@gmail.com>
2024-02-28 16:27:33 +0700

    style: versi desktop

--- pages/portofolio/index.vue
+++ pages/portofolio/index.vue
@@ -1,8 +1,21 @@
+<script setup>
+const items = [
+    '/images/image-1.jpeg',
+    '/images/image-2.jpeg',
+    '/images/image-3.jpeg',
+    '/images/image-4.jpeg',
+    'https://picsum.photos/600/800?random=3',
+    'https://picsum.photos/600/800?random=4',
+    'https://picsum.photos/600/800?random=5',
+    'https://picsum.photos/600/800?random=6'
+]
+</script>
+
 <template>
     <div class="max-h-screen bg-[#DFF8FD]">
         <div class="h-screen">
             <Navbar />
-            <div>
+            <div class="md:hidden">
                 <div class="py-2 text-center font-semibold text-base">Highlight Project</div>
                 <div class=" grid grid-cols-2 mx-2 gap-2">
                     <div class="grid gap-1">
@@ -26,6 +39,12 @@
                         </div>
                     </div>
                 </div>
+            </div>
+            <div class="hidden md:flex md:flex-col md:justify-center h-full">
+                <div class="py-2 text-center font-semibold text-base">Highlight Project</div>
+                <UCarousel v-slot="{ item }" :items="items" :ui="{ item: 'snap-start' }">
+                    <img :src="item" width="300" height="400">
+                </UCarousel>
             </div>
         </div>
     </div>
