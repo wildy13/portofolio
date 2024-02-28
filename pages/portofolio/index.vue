25d6c3a1a853e6d5de9685b773c3c956f77a2353
wildy13 <wildy.simanjuntak13@gmail.com>
2024-02-28 14:39:51 +0700

    style: change size image

--- pages/portofolio/index.vue
+++ pages/portofolio/index.vue
@@ -1,25 +1,25 @@
 <template>
-    <div class="min-h-screen bg-[#DFF8FD] py-8">
-        <div class="h-full">
+    <div class="max-h-screen bg-[#DFF8FD] py-8">
+        <div class="h-screen">
             <div class="py-2 text-center font-semibold text-base">Highlight Project</div>
-            <div class=" grid grid-cols-2 mx-2 gap-2">
-                <div class="grid gap-2">
+            <div class=" grid grid-cols-2 mx-4 gap-0">
+                <div class="grid gap-1">
                     <div>
-                        <img class="h-auto max-w-full rounded-lg"
+                        <img class="h-auto max-w-[90%] rounded-lg"
                             src="/images/image-1.jpeg">
                     </div>
                     <div>
-                        <img class="h-auto max-w-full rounded-lg"
+                        <img class="h-auto max-w-[90%] rounded-lg"
                             src="/images/image-2.jpeg">
                     </div>
                 </div>
-                <div class="grid gap-2">
+                <div class="grid gap-1">
                     <div>
-                        <img class="h-auto max-w-full rounded-lg"
+                        <img class="h-auto max-w-[90%] rounded-lg"
                             src="/images/image-3.jpeg">
                     </div>
                     <div>
-                        <img class="h-auto max-w-full rounded-lg"
+                        <img class="h-auto max-w-[90%] rounded-lg"
                             src="/images/image-4.jpeg">
                     </div>
                 </div>
