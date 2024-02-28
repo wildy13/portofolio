37306f1eb4d0c28323fee650ffddd1d5e5941c5a
wildy13 <wildy.simanjuntak13@gmail.com>
2024-02-28 14:03:57 +0700

    build:  galery

--- pages/portofolio/index.vue
+++ pages/portofolio/index.vue
@@ -1,7 +1,32 @@
 <template>
-    <div class="min-h-screen bg-[#DFF8FD]">
+    <div class="min-h-screen bg-[#DFF8FD] py-8">
         <div class="h-screen">
-            Galery
+            <div class="py-2 text-center font-semibold text-base">Highlight Project</div>
+            <div class=" grid grid-cols-2 mx-2 gap-2">
+                <div class="grid gap-2">
+                    <div>
+                        <img class="h-auto max-w-full rounded-lg"
+                            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="">
+                    </div>
+                    <div>
+                        <img class="h-auto max-w-full rounded-lg"
+                            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="">
+                    </div>
+                </div>
+                <div class="grid gap-2">
+                    <div>
+                        <img class="h-auto max-w-full rounded-lg"
+                            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="">
+                    </div>
+                    <div>
+                        <img class="h-auto max-w-full rounded-lg"
+                            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="py-4 text-center">
+                <nuxtLink to="https://github.com/wildy13/">More</nuxtLink>
+            </div>
         </div>
     </div>
 </template>
