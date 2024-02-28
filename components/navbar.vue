53764a63574529662de7771ee87efb19fe01c377
wildy13 <wildy.simanjuntak13@gmail.com>
2024-02-28 09:42:13 +0700

    style: remove position

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="fixed top-0 left-0 z-50 bg-[#DFF8FD] w-full p-2 flex space-x-4 justify-end">
+    <div class="bg-[#DFF8FD] w-full p-2 flex space-x-4 justify-end">
         <nuxtLink v-motion :initial="{ x: -100, opacity: 0, }" :enter="{ x: 0, opacity: 1, transition: { duration: 1200,  type: 'keyframes', ease: 'easeOut'}}" to="/" class="text-black">Home</nuxtLink>
         <nuxtLink v-motion :initial="{ x: -100, opacity: 0, }" :enter="{ x: 0, opacity: 1, transition: { duration: 900,  type: 'keyframes', ease: 'easeOut'}}" to="/about"  class="text-black">About Me</nuxtLink>
         <nuxtLink v-motion :initial="{ x: -100, opacity: 0, }" :enter="{ x: 0, opacity: 1, transition: { duration: 600,  type: 'keyframes', ease: 'easeOut'}}" to="/contact"  class="text-black">Contact</nuxtLink>
